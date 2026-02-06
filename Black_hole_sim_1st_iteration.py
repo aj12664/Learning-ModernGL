e1798792375770ac52496dfb7fe6a3414f042ff4
aj12664 <alj12664@hotmail.com>
2026-02-06 23:25:38 +0000

    Commit 2 Added camera movement
    Adding camera movement with delta time to stay consistent with speed and somehow made the circle look 3d

--- Black_hole_sim_1st_iteration.py
+++ Black_hole_sim_1st_iteration.py
@@ -3,19 +3,23 @@ import moderngl
 import glfw
 import glm
 import numpy as np
+import time
 
 Width = 1600
 Height = 800
+
 Camera_pos = glm.vec3(0, 0, 50)
-Camera_forward = glm.vec3(0, 0,-1)
+Camera_side = glm.vec3(1, 0, 0)
 Camera_up = glm.vec3(0, 1, 0)
-Radius = 75
+Camera_Speed = 0.01
+
+Radius = 3
 DeltaTheta = 0.05/Radius
 
 if not glfw.init():
     raise RuntimeError("GLFW failed to intialise")
 
-window = glfw.create_window(Width, Height,"Black Hole Simulator",None, None,)
+window = glfw.create_window(Width, Height,"Black Hole Simulator",None, None)
 
 glfw.make_context_current(window)
 
@@ -40,16 +44,13 @@ void main(){
 prog = ctx.program(vertex_shader=vertex_shader_source, fragment_shader=fragment_shader_source)
 
 vertices = [0.0, 0.0, 0.0]
-
 segments = int(tau / DeltaTheta)
 theta_values = np.linspace(0, tau, segments, endpoint=True)
 
 for theta in theta_values:
     x = Radius * np.cos(theta)
     y = Radius * np.sin(theta)
-    x_ndc = x / (Width / 2)
-    y_ndc = y / (Height / 2)
-    vertices.extend([x_ndc, y_ndc, 0.0])
+    vertices.extend([x, y, 0.0])
 
 
 vertices = np.array(vertices, dtype="f4")
@@ -57,11 +58,36 @@ vertices = np.array(vertices, dtype="f4")
 vbo = ctx.buffer(vertices.tobytes())
 vao = ctx.simple_vertex_array(prog, vbo, "in_position")
 
-prog["mvp"].write(np.eye(4, dtype="f4").tobytes())
-
-while not glfw.window_should_close(window):
-    ctx.clear(0.02, 0.02, 0.05)
+def Camera_Movement(window, Camera_side, Camera_up, Camera_pos, speed, dt):
+    if glfw.get_key(window, glfw.KEY_W) == glfw.PRESS:
+        Camera_pos += Camera_up * speed * dt
         
+    if glfw.get_key(window, glfw.KEY_S) == glfw.PRESS:
+        Camera_pos -= Camera_up * speed * dt
+        
+    if glfw.get_key(window, glfw.KEY_D) == glfw.PRESS:
+        Camera_pos += Camera_side * speed * dt
+        
+    if glfw.get_key(window, glfw.KEY_A) == glfw.PRESS:
+        Camera_pos -= Camera_side * speed * dt
+        
+    return Camera_pos
+projection = glm.perspective(glm.radians(45), Width / Height, 0.1, 1000.0)
+
+
+last_time = time.time()
+        
+while not glfw.window_should_close(window):
+    current_time = time.time()
+    dt = current_time - last_time
+    ctx.clear(0.02, 0.02, 0.05)
+    
+    Camera_pos = Camera_Movement(window, Camera_side, Camera_up, Camera_pos, Camera_Speed, dt)
+    view = glm.lookAt(Camera_pos, Camera_pos + glm.vec3(0, 0, -1), Camera_up)
+    
+    mvp = projection * view * glm.mat4(1.0)
+    prog["mvp"].write(np.array(mvp.to_list(), dtype="f4").tobytes())
+
     vao.render(mode=moderngl.TRIANGLE_FAN)
     glfw.poll_events()
     glfw.swap_buffers(window)
